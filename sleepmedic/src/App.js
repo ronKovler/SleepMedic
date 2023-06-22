0dd0e39e7c90ac24adc7bca28f81ee9edd056671
Shaun Thomas <sthomash@costco.com>
2023-06-22 00:58:50 -0700

    Linking HomePage --> Profile Page --> Edit Goals --> Create Reminder Page

--- sleepmedic/src/App.js
+++ sleepmedic/src/App.js
@@ -4,6 +4,8 @@ import {Routes, Route } from 'react-router-dom';
 import LoginPage from './Components/LoginPage';
 import CreateAccount from './Components/CreatePage';
 import Welcome from './Components/Welcome';
+import OpenHomePage from './Components/HomePage';
+import OpenProfilePage from './Components/ProfilePage';
 import EditGoal from './Components/EditGoalPage';
 import CreateRem from './Components/CreateReminderPage';
 import React from 'react';
@@ -16,6 +18,8 @@ function App() {
         <Route path="/" element={<Welcome />} />
         <Route path="/login" element={<LoginPage/>} />
         <Route path="/createaccount" element={<CreateAccount/>} />
+        <Route path="/homepage" element={<OpenHomePage>} />
+        <Route path="/profilepage" element={<OpenProfilePage>} />
         <Route path="/editgoal" element={<EditGoal/>} />
         <Route path="/createreminder" element={<CreateRem/>} />
 
