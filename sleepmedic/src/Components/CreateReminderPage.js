d1564b34c679a44874d51085c171c6ee9fb92d34
Shaun Thomas <sthomash@costco.com>
2023-06-28 23:45:55 -0700

    Planning out input validation for ReminderType, selectedDays, and ReminderTime. Triggered on handleCreateReminder() function.

--- sleepmedic/src/Components/CreateReminderPage.js
+++ sleepmedic/src/Components/CreateReminderPage.js
@@ -4,7 +4,6 @@ import {TextField, Select, MenuItem, Button } from "@mui/material/";
 import axios from "axios";
 import { valueToPercent } from '@mui/base';
 import React, { useState } from 'react';
-//import React from 'react';
 
 
 function getCookiesDict() {
@@ -18,74 +17,97 @@ function getCookiesDict() {
 
 //Shaun
 function CreateRem() {
-    const [ReminderType, changeRemType] = useState("None");
+    const [ReminderType, setRemType] = useState("None");
     const [ReminderTime, setRemTime] = useState('');
     const [ReminderName, setRemName] = useState('');
     const daysOfWeek = [{day: "Sun"}, {day:"Mon"}, {day:"Tues"}, {day:"Wed"}, {day:"Thu"}, {day:"Fri"}, {day:"Sat"}];
     const [checkedState, setCheckedState] = useState(
         new Array(daysOfWeek.length).fill(false)
     );
-    //handleChange() for checkboxes
-    const handleOnChange = (position) => {
+
+    //handleChange() method for checkboxes: maintains which days are selected by the user
+    const handleOnChangeCB = (position) => {
         const updatedCheckedState = checkedState.map((item, index) =>
           index === position ? !item : item
         );
         setCheckedState(updatedCheckedState);
     };
 
+
+    /* dayInputValidation, set error message accordingly
+        at least one day (checkbox) should be selected.
+    */
+
+    /* Time input validation, set error message accordingly
+        Should be in format: 00:00PM or AM
+        if input is empty, prompt error message
+        if input does not contain :, prompt error message
+        if input does not have hours and minute in format of 00:00, prompt error message
+        if input does not end in PM or AM, prompt error message.
+    */
+
+    //TODO: input validation - are days selected? is there a time specified? Has it been inputted in the correct format?
+    /* Main input validation
+        call reminderTypeInputValidation, set error message accordingly
+        call dayInputValidation, set error message accordingly
+        call timeInputValidation, set error message accordingly
+        Error messages should be displayed on the right side of their respective component
+    */
+    const validate = () => {
+        //call your specific input functions here.
+        return true;
+    }
+
     const handleCreateReminder = async(e) => {
-        e.preventDefault();
-        const cookies = getCookiesDict();
-        let tok = cookies._auth;
-        console.log('Create button clicked');
-        //grabbing reminderType
-        let reminderTypeInt;
-        if (ReminderType == "Bedtime Reminder") {
-            reminderTypeInt = 1;
-        } else if (ReminderType === "Sleep Hygiene Reminder") {
-            reminderTypeInt = 2;
-        } else {
-            reminderTypeInt = 0; // Default value when ReminderType is "None"
-        }
-        //convert ReminderTime to the required format (hr-min)
-        const [time, clock] = ReminderTime.split(/(?<=[0-9]{2})(?=[AP]M)/);
-        const [hours, minutes] = time.split(":");
-        let formattedHours = parseInt(hours, 10); // Parse hours as integer
-        if (clock === "PM") {
-          if (formattedHours !== 12) {
-            formattedHours += 12; // Add 12 hours for PM format (except when it's 12PM)
-          }
-        } else if (formattedHours === 12) {
-          formattedHours = 0; // Convert 12 AM to 0 hours
-        }
-        const formattedMinutes = minutes.padStart(2, '0'); // Pad minutes with leading zero if necessary
-        const formattedReminderTime = `${formattedHours}:${formattedMinutes}:00`;
-        //grabbing the days selected by user
-        const selectedDays = checkedState
-            .map((isChecked, index) => (isChecked ? index : null))
-            .filter((day) => day !== null);
-        //headers
-        var headers = {
+        if (validate()) {
+            e.preventDefault();
+            const cookies = getCookiesDict();
+            let tok = cookies._auth;
+            console.log('Create button clicked');
+            //grabbing reminderType
+            let reminderTypeInt;
+            if (ReminderType == "Bedtime Reminder") {
+                reminderTypeInt = 1;
+            } else if (ReminderType === "Sleep Hygiene Reminder") {
+                reminderTypeInt = 2;
+            } else {
+                reminderTypeInt = 0; // Default value when ReminderType is "None"
+            }
+            //grab ReminderTime and convert to the required format (hr:min:sec) (military)
+            const [time, clock] = ReminderTime.split(/(?<=[0-9]{2})(?=[AP]M)/);
+            const [hours, minutes] = time.split(":");
+            let formattedHours = parseInt(hours, 10); // Parse hours as integer
+            if (clock === "PM") {
+                if (formattedHours !== 12) {
+                    formattedHours += 12; // Add 12 hours for PM format (except when it's 12PM)
+                }
+            } else if (formattedHours === 12) {
+                formattedHours = 0; // Convert 12 AM to 0 hours
+            }
+            const formattedMinutes = minutes.padStart(2, '0'); // Pad minutes with leading zero if necessary
+            const formattedReminderTime = `${formattedHours}:${formattedMinutes}:00`;
+            //grabbing the days selected by user
+            const selectedDays = checkedState
+                .map((isChecked, index) => (isChecked ? index : null))
+                .filter((day) => day !== null);
+
+            var headers = {
                 "Access-Control-Allow-Origin": "http://ec2-18-222-211-114.us-east-2.compute.amazonaws.com:8080/",
                 "Authorization":'Bearer ' + tok
+            }
+            var reminderInfo = {
+                triggerTime: formattedReminderTime, //Time at which reminder emails will be triggered on the chosen days
+                triggerDays: selectedDays,          //a list of integers where 0 is Sun, 6 is Sat
+                message: reminderTypeInt,           //1 or 2; Bedtime or General Sleep Reminder
+            }
+            try {
+                let res = await axios.post("http://ec2-18-222-211-114.us-east-2.compute.amazonaws.com:8080/api/reminder/create_reminder", reminderInfo, {headers});
+                console.log(res);
+            }
+            catch (err) {
+                console.log("Failed to send CreateReminder data.");
+            }
         }
-        var reminderInfo = {
-            //time: hr-min
-            triggerTime: formattedReminderTime,
-            //days - as list of integers where 1 is Monday.
-            triggerDays: selectedDays,
-            //ReminderType ; 1-2 (Bedtime or General Reminder)
-            message: reminderTypeInt,
-            /////all in JSON message
-        }
-        try {
-               //this is supposed to be POST right?
-               let res = await axios.post("http://ec2-18-222-211-114.us-east-2.compute.amazonaws.com:8080/api/reminder/create_reminder", reminderInfo, {headers});
-               console.log(res);
-        }
-       catch (err) {
-            console.log("Failed to send CreateReminder data.");
-       }
     };
     return (
      <div className="sleep-medic-container">
@@ -99,7 +121,7 @@ function CreateRem() {
              id="reminder-type"
              value={ReminderType}
              label="Reminder Type"
-             onChange={(e) => changeRemType(e.target.value)}   //do onChange={(e) => stuff
+             onChange={(e) => setRemType(e.target.value)}    //TODO: implement informational box rendering.
             style={{ width: "230px" }} >
              <MenuItem value="None">None</MenuItem>
              <MenuItem value="Bedtime Reminder">Bedtime Reminder</MenuItem>
@@ -117,7 +139,7 @@ function CreateRem() {
                         name={day}
                         value={day}
                         checked={checkedState[index]}
-                        onChange={() => handleOnChange(index)}
+                        onChange={() => handleOnChangeCB(index)}
                     />
                     <label htmlFor={`custom-checkbox-${index}`}>{day}</label>
                 </div>
