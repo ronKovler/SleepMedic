845dcbea9669523604426051d5eb8d83d08290e8
Ron Kovler <kovler.ron@gmail.com>
2023-07-15 02:56:49 -0700

    fixed bugs on frontend and backend with formatting

--- sleepmedic/src/Components/home/Home.js
+++ sleepmedic/src/Components/home/Home.js
@@ -265,11 +265,14 @@ export default function Home() {
         time = time.slice(0, 5);
         let hours = parseInt(time.slice(0, 2), 10);
         let remainder = time.slice(2);
-        if (hours > 12) {
-            hours -= 12
-            return hours.toString() + remainder + " PM";
+        if (hours > 22) {
+            hours -= 12;
+            return hours + remainder + " PM";
+        } else if (hours > 12) {
+            hours -= 12;
+            return "0" + hours + remainder + " PM";
         }
-        return hours.toString + remainder + " AM";
+        return hours + remainder + " AM";
     }
 
     async function getData() {
